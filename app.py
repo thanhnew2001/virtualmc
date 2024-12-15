9a4135cdbc3f1057a9163cbab7d68ee4ee7c58a0
thanhnew2001 <69960815+thanhnew2001@users.noreply.github.com>
2024-12-15 19:38:13 +1100

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def upload_files():
             print("url", output)
        
             # Return the video URL as JSON response
-            return jsonify({'video_url': 'output'})
+            return jsonify({'video_url': output.Output})
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
@@ -98,7 +98,7 @@ def upload_files():
             print("url", output)
    
             # Return the video URL as JSON response
-            return jsonify({'video_url': ''})
+            return jsonify({'video_url': output.Output})
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
